c1610cb8676880d2938636d2fd8e5b82a8e18bb4
wabomba-bakar <wabombabakar@gmail.com>
2019-11-05 02:28:17 +0300

    deploy

--- utils/db.js
+++ utils/db.js
@@ -3,4 +3,5 @@ if (process.env.NODE_ENV === 'production')
     mongoURI:
       'mongodb://<bakar>:<0785351645wb>@ds243148.mlab.com:43148/electronic-shop'
   };
+
 module.exports = { mongoURI: 'mongodb://localhost:27017/node-shop' };
