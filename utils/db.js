7367bf749d29b2ebe64551a9cbe94984c6e17bad
wabomba-bakar <wabombabakar@gmail.com>
2019-11-05 01:39:41 +0300

    fixed database connection error.

--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,4 @@
-if (process.env.NODE_ENV === 'production')
-  module.exports = {
-    mongoURI:
-      'mongodb://<bakar>:<0785351645wb>@ds243148.mlab.com:43148/electronic-shop'
-  };
-module.exports = { mongoURI: 'mongodb://localhost:27017/node-shop' };
+module.exports = {
+  mongoURI:
+    'mongodb://<bakar>:<0785351645wb>@ds243148.mlab.com:43148/electronic-shop'
+};
