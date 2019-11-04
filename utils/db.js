aa744d5de193b77fe40f83aaae74bdde904400c7
wabomba-bakar <wabombabakar@gmail.com>
2019-11-05 01:01:51 +0300

    added remote database.

--- utils/db.js
+++ utils/db.js
@@ -1,2 +1,6 @@
-if (process.env.NODE_ENV === "production") module.exports = { mongoURI: "" };
-module.exports = { mongoURI: "mongodb://localhost:27017/node-shop" };
+if (process.env.NODE_ENV === 'production')
+  module.exports = {
+    mongoURI:
+      'mongodb://<bakar>:<0785351645wb>@ds243148.mlab.com:43148/electronic-shop'
+  };
+module.exports = { mongoURI: 'mongodb://localhost:27017/node-shop' };
