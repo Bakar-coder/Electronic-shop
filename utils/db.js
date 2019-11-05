03f635aae2a418dd12c93fffbf4d60ff1ee95498
wabomba-bakar <wabombabakar@gmail.com>
2019-11-05 03:39:18 +0300

    set database environment valiable

--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,7 @@
+const config = require('config');
+
 if (process.env.NODE_ENV === 'production')
   module.exports = {
-    mongoURI:
-      'mongodb://bakar:0785351645wb@ds243148.mlab.com:43148/electronic-shop'
+    mongoURI: config.get('database')
   };
 module.exports = { mongoURI: 'mongodb://localhost:27017/node-shop' };
