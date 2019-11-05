f9a8914bd23e0ff1e83a921553fdf7d118fc618f
wabomba-bakar <wabombabakar@gmail.com>
2019-11-05 03:19:12 +0300

    confired remote database

--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,6 @@
 if (process.env.NODE_ENV === 'production')
   module.exports = {
     mongoURI:
-      'mongodb://<bakar>:<0785351645wb>@ds243148.mlab.com:43148/electronic-shop'
+      'mongodb://bakar:0785351645wb@ds243148.mlab.com:43148/electronic-shop'
   };
-
 module.exports = { mongoURI: 'mongodb://localhost:27017/node-shop' };
